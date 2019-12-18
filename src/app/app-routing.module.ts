dbe7cc8c80da14c48e222858b3fe942b49d6a5f6
krish <krishsrivatsav@gmail.com>
2019-12-18 10:39:47 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { componentFactoryName } from '@angular/compiler';
+
+
+const routes: Routes = [
+// {
+//   path: '',
+//   component : DshbComponent
+// }
+// {
+//   path : 'user';
+//   component : UserComponent
+// }
+];
+
+  @NgModule{
+  // imports: [RouterModule.forRoot(routes)],
+  // exports: [RouterModule]
+}
+export class AppRoutingModule { }
---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  min = true;
+  dat = true;
+  title = 'MyFirstAngularProject';
+  // cards = [
+  //   {
+  //     period: 'monthly',
+  //     border: 'danger',
+  //     price: '10000',
+  //     currency: 'yen',
+  //   },
+  //   {
+  //     period: 'monthly',
+  //     border: 'danger',
+  //     price: '20000',
+  //     currency: 'Euro',
+  //   },
+  //   {
+  //     period: 'monthly',
+  //     border: 'danger',
+  //     price: '30000',
+  //     currency: 'DOLLAR',
+  //   },
+  //   {
+  //     period: 'monthly',
+  //     border: 'success',
+  //     price: '40000',
+  //     currency: 'RS',
+  //   }
+  // ]
+
+    
+}
